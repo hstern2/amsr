b361b21aeff45e7951b16647d6a0f5465d76fc71
HARRY A STERN <harry.stern@rochester.edu>
2022-01-27 22:41:04 -0500

    no __all__

--- amsr/__init__.py
+++ amsr/__init__.py
@@ -1,19 +1,6 @@
 from .version import __version__
-
-__all__ = [
-    "FromMol",
-    "FromMolToTokens",
-    "FromSmiles",
-    "FromSmilesToTokens",
-    "ToMol",
-    "ToSmiles",
-    "CheckMol",
-    "CheckSmiles",
-    "Groups",
-    "InitializeGroups",
-]
-
 from .encode import FromMol, FromMolToTokens, FromSmiles, FromSmilesToTokens
 from .decode import ToMol, ToSmiles
 from .check import CheckMol, CheckSmiles
 from .groups import Groups, InitializeGroups
+from .tokens import ToTokens
