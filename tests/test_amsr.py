ffd76378270cda6d31cab6ce2135364d424ef60d
Harry Stern <madayrrah16@gmail.com>
2023-11-20 20:33:36 -0500

    caffeine

--- tests/test_amsr.py
+++ tests/test_amsr.py
@@ -10,6 +10,10 @@ def test_methane():
     assert amsr.CheckSmiles("C")
 
 
+def test_caffeine():
+    assert amsr.CheckSmiles("CN1C=NC2=C1C(=O)N(C(=O)N2C)C")
+
+
 def test_taxol():
     assert amsr.CheckSmiles(
         "CC1=C2[C@@]([C@]([C@H]([C@@H]3[C@]4([C@H](OC4)C[C@@H]([C@]3(C(=O)[C@@H]2OC(=O)C)C)O)OC(=O)C)OC(=O)c5ccccc5)(C[C@@H]1OC(=O)[C@H](O)[C@@H](NC(=O)c6ccccc6)c7ccccc7)O)(C)C"
