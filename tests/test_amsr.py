d4cc649247143da94640441d818418535884eb86
Harry Stern <madayrrah16@gmail.com>
2023-12-15 12:41:23 -0500

    test_sampler

--- tests/test_amsr.py
+++ tests/test_amsr.py
@@ -1,17 +1,18 @@
-import amsr, os
-import pandas as pd
+import amsr, os, pandas
+from random import seed
+from rdkit import Chem
 
 
 def test_version():
     assert amsr.__version__
 
 
+def _read_csv(csv_file):
+    return pandas.read_csv(os.path.join(os.path.dirname(__file__), csv_file))
+
+
 def _test_csv(csv_file):
-    assert (
-        pd.read_csv(os.path.join(os.path.dirname(__file__), csv_file))
-        .apply(lambda m: amsr.CheckSmiles(m.SMILES), axis=1)
-        .all()
-    )
+    assert _read_csv(csv_file).apply(lambda m: amsr.CheckSmiles(m.SMILES), axis=1).all()
 
 
 def test_Mg():
@@ -24,3 +25,11 @@ def test_NP():
 
 def test_FDA():
     _test_csv("some_FDA_approved_structures.csv")
+
+
+def test_sampler():
+    seed(0)
+    df = _read_csv("some_FDA_approved_structures.csv")
+    sampler = amsr.Sampler((Chem.MolFromSmiles(s) for s in df["SMILES"]))
+    for _ in range(1000):
+        assert amsr.CheckMol(amsr.ToMol(sampler.sample(20)))
