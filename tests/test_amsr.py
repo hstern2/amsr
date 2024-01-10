99bf4fbe07bc996979e6485b7f1046addd03c8ca
Harry Stern <madayrrah16@gmail.com>
2024-01-10 08:44:03 -0500

    changes

--- tests/test_amsr.py
+++ tests/test_amsr.py
@@ -15,10 +15,6 @@ def _test_csv(csv_file):
     assert _read_csv(csv_file).apply(lambda m: amsr.CheckSmiles(m.SMILES), axis=1).all()
 
 
-def test_elements():
-    _test_csv("elements.csv")
-
-
 def test_Mg_compounds():
     _test_csv("Mg-compounds.csv")
 
@@ -29,11 +25,6 @@ def test_NP():
 
 def test_FDA():
     _test_csv("some_FDA_approved_structures.csv")
-
-
-# def test_chembl_33():
-#    df = pandas.read_csv(os.path.join(os.path.dirname(__file__), "chembl_33.smi"), delimiter=" ")
-#    assert(df.apply(lambda m: amsr.CheckSmiles(m.canonical_smiles), axis=1).all())
 
 
 def test_sampler():
