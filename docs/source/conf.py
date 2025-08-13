70b4a9cfdce4caa6f2c06263a3d315f149305742
Harry Stern <madayrrah16@gmail.com>
2025-08-13 16:33:36 -0400

    docs conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,6 +1,8 @@
 # amsr/docs/source/conf.py
 
-import os, sys
+import os
+import sys
+
 sys.path.insert(0, os.path.abspath("../../"))
 
 project = "amsr"
@@ -8,8 +10,8 @@ author = "Harry Stern"
 release = "0.1.4"
 
 extensions = [
-    "sphinx.ext.autodoc",       # <-- must precede sphinx_autodoc_typehints
-    "sphinx.ext.napoleon",      # (optional) Google/NumPy docstrings
+    "sphinx.ext.autodoc",  # <-- must precede sphinx_autodoc_typehints
+    "sphinx.ext.napoleon",  # (optional) Google/NumPy docstrings
     "myst_parser",
     "sphinx_autodoc_typehints",
 ]
@@ -19,11 +21,9 @@ source_suffix = {
     ".md": "markdown",
 }
 
-templates_path = ["_templates"]
-exclude_patterns = []
-
-html_theme = "furo"
-html_static_path = []  # keep empty unless you create _static
+templates_path: list[str] = ["_templates"]
+exclude_patterns: list[str] = []
+html_static_path: list[str] = []  # keep empty unless you create _static
 
 # (optional) nice defaults
 autodoc_typehints = "description"
